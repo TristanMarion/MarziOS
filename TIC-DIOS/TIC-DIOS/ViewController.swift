89642d677b374ab309d576bebc4d6fb77a23b0c1
Tristan Marion <trismarion@gmail.com>
2017-07-19 09:37:24 +0200

    Gesture pan moving Marzi

--- TIC-DIOS/TIC-DIOS/ViewController.swift
+++ TIC-DIOS/TIC-DIOS/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var marzi: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +20,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func handlePan(_ gestureRecognizer: UIPanGestureRecognizer) {
+        if gestureRecognizer.state == .began || gestureRecognizer.state == .changed {
+            
+            let translation = gestureRecognizer.translation(in: self.view)
+            marzi.center = CGPoint(x: marzi.center.x + translation.x * 1.5, y: marzi.center.y + translation.y * 1.5)
+            gestureRecognizer.setTranslation(CGPoint.zero, in: self.view)
+        }
+    }
 }
 
