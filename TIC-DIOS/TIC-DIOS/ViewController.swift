36fa12b58ad3b6c3e96c5a440a12d975e3fc047e
Tristan Marion <trismarion@gmail.com>
2017-07-19 10:01:01 +0200

    Marzi now stays in the view even if he tries to go out of it

--- TIC-DIOS/TIC-DIOS/ViewController.swift
+++ TIC-DIOS/TIC-DIOS/ViewController.swift
@@ -23,9 +23,24 @@ class ViewController: UIViewController {
     
     @IBAction func handlePan(_ gestureRecognizer: UIPanGestureRecognizer) {
         if gestureRecognizer.state == .began || gestureRecognizer.state == .changed {
-            
             let translation = gestureRecognizer.translation(in: self.view)
-            marzi.center = CGPoint(x: marzi.center.x + translation.x * 1.5, y: marzi.center.y + translation.y * 1.5)
+            
+            let marzi_semi_width = marzi.frame.width / 2
+            let marzi_semi_height = marzi.frame.height / 2
+            
+            // Marzi is faster than your finger
+            var pos_x = marzi.center.x + translation.x * 1.5
+            var pos_y = marzi.center.y + translation.y * 1.5
+            
+            // if Marzi is not at a good x value
+            pos_x = pos_x < marzi_semi_width ? marzi_semi_width : pos_x
+            pos_x = pos_x > view.frame.width - marzi_semi_width ? view.frame.width - marzi_semi_width : pos_x
+            
+            // if Marzi is not at a good y value
+            pos_y = pos_y < marzi_semi_height ? marzi_semi_height : pos_y
+            pos_y = pos_y > view.frame.height - marzi_semi_height ? view.frame.height - marzi_semi_height : pos_y
+            
+            marzi.center = CGPoint(x: pos_x, y: pos_y)
             gestureRecognizer.setTranslation(CGPoint.zero, in: self.view)
         }
     }
